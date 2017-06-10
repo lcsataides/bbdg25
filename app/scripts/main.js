2abe877ab2a59f5f9fbf3f352b0ad11030b31115
Lucas Ataides <lcsataides@gmail.com>
2017-06-10 16:12:48 -0300

    Added scrollmagic plugin, small changes in modal.scss and url update in entrance

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ import {} from './external/jquery';
 import {} from 'foundation-sites'; // eslint-disable-line import/imports-first
 import {} from 'slick-carousel'; // eslint-disable-line import/imports-first
 import {} from 'fullpage.js'; // eslint-disable-line import/imports-first
+import {} from 'scrollmagic'; // eslint-disable-line import/imports-first
 import {} from 'gsap'; // eslint-disable-line import/imports-first
 import { slick } from './external/slick';
 
